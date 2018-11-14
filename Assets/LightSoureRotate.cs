6127aa40aa56e75d70c2be426e179b889016f8a8
Shicheng Chu <cheng61746093@gmail.com>
2018-11-13 16:25:32 -0800

    add rotation to directionl light

---
+++ Assets/LightSoureRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSoureRotate : MonoBehaviour {
+    public GameObject light;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = light.transform.eulerAngles.x;
+        float y = light.transform.eulerAngles.y;
+        float z = light.transform.eulerAngles.z;
+
+        light.transform.eulerAngles=new Vector3(x,y+0.5f,z);
+	}
+}
