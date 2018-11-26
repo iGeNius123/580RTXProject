786d5bcd41e3a2df9bcdea0be4f8e969696b0bfb
Kyle Tang <ankaphantom0223@gmail.com>
2018-11-27 05:45:41 +0800

    photonmapping
    photonmapping

--- Assets/PhotonMappingMaster.cs
+++ Assets/PhotonMappingMaster.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class RayTracingMaster : MonoBehaviour
 {
     public ComputeShader RayTracingShader;
     public Texture SkyboxTexture;
     public Light DirectionalLight;
-    public Light PointLight;
+    public static Light PointLight;
 
     [Header("Spheres")]
     public Vector2 SphereRadius = new Vector2(3.0f, 8.0f);
@@ -21,8 +23,9 @@ public class RayTracingMaster : MonoBehaviour
     private ComputeBuffer _sphereBuffer;
     private List<Transform> _transformsToWatch = new List<Transform>();
 
-    private List<Sphere> spheres  = new List<Sphere>();
-
+    private List<Sphere> spheres = new List<Sphere>();
+   
+    
     struct Sphere
     {
         public int id;
@@ -36,221 +39,263 @@ public class RayTracingMaster : MonoBehaviour
     // photon mapping ---------------------------------------------------------
     struct Photon
     {
-      float[3] position;
-      float[3] direction;
-      float[3] energy;
-      int sphere_id;
+        public Vector3 position;
+        public Vector3 direction;
+        public Vector3 energy;
+        public int sphere_id;
     }
+
+    Photon p = new Photon();
+    Sphere oo = new Sphere();
 
     private Vector3 lightPos = PointLight.transform.position;
 
     private int numPhotons = 1000; // total number of photons emitted
     private int numBounces = 3; // max number of times each photon bounces
-    private float sqRadius = 0.7;
-    private float exposure = 50.0;
-
+    private float sqRadius = 0.7f;
+    private float exposure = 50.0f;
+    //Photon p = new Photon();
     private List<Photon> gPhotonMap = new List<Photon>();
 
-    private gIntersect = false; // did photon intersect anything?
+    private bool gIntersect = false; // did photon intersect anything?
     private int gIndex = -1; // id of intersected sphere
-    private float gSqDist, gDist = -1.0; // distance from ray to intersection
-    private float[3] gPoint = {0.0, 0.0, 0.0}; // point of intersection
+    private float gSqDist, gDist = -1.0f; // distance from ray to intersection
+    private float[] gPoint = { 0.0f, 0.0f, 0.0f }; // point of intersection
 
     // Function to find sphere intersection
     // ray = location of photon, origin = location of point light
-    private void raySphere (Sphere other, float[] ray, float[] origin)
+    private void intersectSphere(Sphere other, float[] ray, float[] origin)
     {
-      float[] s = substractXYZ (other.position, origin);
-      float radius = other.radius;
-      // quadratic function for Distance
-      float A = dotProduct (ray, ray);
-  		float B = -2.0f * dotProduct (raySphere, ray);
-  		float C = dotProduct (raySphere, raySphere) - (radius * radius);
+        //Vector3 s = substractXYZ (other.position, origin);
+        float[] s = { 0.0f, 0.0f, 0.0f };
+        s[0] = other.position.x - origin[0];
+        s[1] = other.position.y - origin[1];
+        s[2] = other.position.z - origin[2];
+        float radius = other.radius;
+        // quadratic function for Distance
+        float A = dotProduct (ray, ray);
+  		float B = -2.0f * dotProduct (s, ray);
+  		float C = dotProduct (s, s) - (radius * radius);
   		float D = B * B - 4 * A * C;
 
   		if (D > 0.0)
-      {
+        {
   			float sign = (C < -0.00001) ? 1 : -1;
   			float distance = (-B + sign * Mathf.Sqrt (D)) / (2 * A);
-        if (distance < gDist && distance > 0.0)
-        {
-          gIndex = other.id;
-          gDist = distance;
-          gIntersect = true;
-        }
+            if (distance < gDist && distance > 0.0)
+            {
+                gIndex = other.id;
+                gDist = distance;
+                gIntersect = true;
+            }
   		}
+    }
+
+    float[] surfaceNormal( int index, float[] P, float[] Inside) { return sphereNormal(index, P); }
+
+    float[] reflect(float[] ray, float[] fromPoint)
+    {                //Reflect Ray
+        float[] N = surfaceNormal(gIndex, gPoint, fromPoint);  //Surface Normal
+        return normalize3(substractXYZ(ray, multiplyThree(N, (2 * dotProduct(ray, N)))));   //Approximation to Reflection
     }
 
     private void rayTrace (float[] ray, float[] origin)
     {
-      gIntersect = false;
-      gDist = 999999.9;
+        gIntersect = false;
+        gDist = 999999.9f;
 
-      foreach (Sphere other in spheres)
-      {
-        intersectSphere(other, ray, origin);
-      }
+        foreach (Sphere other in spheres)
+        {
+            intersectSphere(other, ray, origin);
+        }
     }
 
     private void emitPhotons ()
     {
       Random.seed = 0;
-      for (int i = 0; i < nrPhotons ; i++)
+      for (int i = 0; i < numPhotons; i++)
       {
-        int bounces = 1;
-        float[] rgb = { 1.0f, 1.0f, 1.0f };
-        float[] ray = normalize3 (randomThree (1.0f));
-        float[] prevPoint = lightPos;
+            int bounces = 1;
+            float[] rgb = { 1.0f, 1.0f, 1.0f };
+            float[] ray = normalize3 (randomThree (1.0f));
+            float[] prevPoint = { 0.0f, 0.0f, 0.0f };
+            prevPoint[0] = lightPos.x;
+            prevPoint[1] = lightPos.y;
+            prevPoint[2] = lightPos.z;
 
-        while (prevPoint [1] >= lightPos [1])
-        {
-          prevPoint = additionXYZ (lightPos, multiplyThree (normalize3 (randomThree (1.0f)), 0.75f));
+            while (prevPoint[1] >= lightPos[1])
+            {
+                prevPoint = additionXYZ (prevPoint, multiplyThree (normalize3 (randomThree (1.0f)), 0.75f));
+            }
+            // todo: we need to set bounds for the photons; change 1.5 and 1.2
+            // this if statement should check if the photon bounces outside the
+            // boundary or inside a sphere
+            // 1.5 is boundary of cornell box?
+            // 1.2 is light source?
+            if (abs (prevPoint [0]) > 1.5 || abs (prevPoint [1]) > 1.2f || findSqureDistance (prevPoint, objects.getSphereObject (0), objects.getSphereData (0, 3) * objects.getSphereData (0, 3)))
+                bounces = numBounces + 1;
+
+            rayTrace (ray, prevPoint);
+
+            while (gIntersect && bounces <= numBounces)
+            {
+                gPoint = additionXYZ (multiplyThree (ray, gDist) , prevPoint);
+                rgb = multiplyThree(getColor(rgb, gIndex), 1.0f / Mathf.Sqrt(bounces));
+                // todo: i think we need to store which sphere this photon is associated with
+                storePhoton (gIndex, gPoint, ray, rgb);
+                //drawPhoton(rgb, gPoint);
+                ray = reflect(ray, prevPoint);
+                rayTrace (ray, gPoint);
+                prevPoint = gPoint;
+                bounces++;
+            }
         }
-        // todo: we need to set bounds for the photons; change 1.5 and 1.2
-        // this if statement should check if the photon bounces outside the
-        // boundary or inside a sphere
-        // 1.5 is boundary of cornell box?
-        // 1.2 is light source?
-        if (abs (prevPoint [0]) > 1.5 || abs (prevPoint [1]) > 1.2f ||
-            findSqureDistance (oldPoint, objects.getSphereObject (0), objects.getSphereData (0, 3) * objects.getSphereData (0, 3)))
-          bounces = numberOfBounces + 1;
-
-        rayTrace (ray, prevPoint);
-
-        while (gIntersect && bounces <= nrBounces)
-        {
-          gPoint = additionXYZ (multiplyThree (ray, gDist) , prevPoint);
-          rgb = multiplyThree (getColor (rgb, gIndex, 1.0f / Mathf.Sqrt (bounces));
-          // todo: i think we need to store which sphere this photon is associated with
-          storePhoton (gIndex, gPoint, ray, rgb);
-          drawPhoton (ray);
-          ray = reflect (ray, prevPoint);
-          raytrace (ray, gPoint);
-          prevPoint = gPoint;
-          bounces++;
-        }
-      }
     }
-
+      
     private void storePhoton (int id, float[] position, float[] direction, float[] energy)
-    {
-      Photon p = new Photon();
-      p.position = position;
-      p.direction = direction;
-      p.energy = energy;
-      p.sphere_id = id;
-      gPhotonMap.add(p);
+    {      
+        p.position.x = position[0];
+        p.position.y = position[1];
+        p.position.z = position[2];
+        p.direction.x = direction[0];
+        p.direction.y = direction[1];
+        p.direction.z = direction[2];
+        p.energy.x = energy[0];
+        p.energy.y = energy[1];
+        p.energy.z = energy[2];
+        p.sphere_id = id;
+        gPhotonMap.Add(p);     
     }
 
+    float min(float a, float b) { return a < b ? a : b; }
 
+    float[] filterColor(float[] rgbIn, float r, float g, float b)
+    { //e.g. White Light Hits Red Wall
+        float[] rgbOut = { r, g, b };
+        for (int c = 0; c < 3; c++) rgbOut[c] = min(rgbOut[c], rgbIn[c]); //Absorb Some Wavelengths (R,G,B)
+        return rgbOut;
+    }
 
-
+    float[] getColor(float[] rgbIn, int index)
+    { //Specifies Material Color of Each Object
+        if (index == 0) { return filterColor(rgbIn, 0.0f, 1.0f, 0.0f); }
+        else if (index == 2) { return filterColor(rgbIn, 1.0f, 0.0f, 0.0f); }
+        else { return filterColor(rgbIn, 1.0f, 1.0f, 1.0f); }
+    }
 
 
     // 3D math functions ------------------------------------------------------
     public float abs (float value)
     {
-      return Mathf.Abs (value);
+        return Mathf.Abs (value);
     }
 
     public float pow (float value, float power)
     {
-      return Mathf.Pow (value, power);
+        return Mathf.Pow (value, power);
     }
 
     public bool odd (int value)
     {
-      return value % 2 != 0;
+        return value % 2 != 0;
     }
 
     public float[] normalize3 (float[] v)
     {
-      Vector3 tmp = new Vector3 (v [0], v [1], v [2]).normalized;
-      v [0] = tmp.x;
-      v [1] = tmp.y;
-      v [2] = tmp.z;
-      return v;
+        Vector3 tmp = new Vector3 (v [0], v [1], v [2]).normalized;
+        v [0] = tmp.x;
+        v [1] = tmp.y;
+        v [2] = tmp.z;
+        return v;
     }
 
     public float[] substractXYZ (float[] a, float[] b)
     {
-      float[] result = { a [0] - b [0], a [1] - b [1], a [2] - b [2] };
-      return result;
+        float[] result = { a [0] - b [0], a [1] - b [1], a [2] - b [2] };
+        return result;
     }
 
     public float[] additionXYZ (float[] a, float[] b)
     {
-      float[] result = { a [0] + b [0], a [1] + b [1], a [2] + b [2] };
-      return result;
+        float[] result = { a [0] + b [0], a [1] + b [1], a [2] + b [2] };
+        return result;
     }
 
     public float[] multiplyThree (float[] a, float c)
     {
-      float[] result = { c * a [0], c * a [1], c * a [2] };
-      return result;
+        float[] result = { c * a [0], c * a [1], c * a [2] };
+        return result;
     }
 
     public float dotProduct (float[] a, float[] b)
     {
-      return a [0] * b [0] + a [1] * b [1] + a [2] * b [2];
+        return a [0] * b [0] + a [1] * b [1] + a [2] * b [2];
     }
 
     public float[] randomThree (float s)
     {
-      float[] rand = { Random.Range (-s, s), Random.Range (-s, s), Random.Range (-s, s) };
-      return rand;
+        float[] rand = { Random.Range (-s, s), Random.Range (-s, s), Random.Range (-s, s) };
+        return rand;
     }
 
     public bool findSqureDistance (float[] a, float[] b, float sqradius)
     {
-      float c = a [0] - b [0];
-      float d = c * c;
-      if (d > sqradius)
-        return false;
-      c = a [1] - b [1];
-      d += c * c;
-      if (d > sqradius)
-        return false;
-      c = a [2] - b [2];
-      d += c * c;
-      if (d > sqradius)
-        return false;
-      variableUtils.setSqDistance (d);
-      return true;
+        float c = a [0] - b [0];
+        float d = c * c;
+        if (d > sqradius)
+            return false;
+        c = a [1] - b [1];
+        d += c * c;
+        if (d > sqradius)
+            return false;
+        c = a [2] - b [2];
+        d += c * c;
+        if (d > sqradius)
+            return false;
+        variableUtils.setSqDistance (d);
+        return true;
     }
 
     public float lightDiffuse (float[] N, float[] P)
     {
-      float[] L = normalize3 (substractXYZ (lightSource, P));
-      return dotProduct (N, L);
+        float[] a = { 0.0f, 0.0f, 0.0f };
+        a[0] = lightPos.x - P[0];
+        a[1] = lightPos.y - P[1];
+        a[2] = lightPos.z - P[2];
+        float[] L = normalize3 (a);
+        return dotProduct (N, L);
     }
 
     public float[] sphereNormal (int idx, float[] P)
     {
-      return normalize3 (substractXYZ (P, objects.getSphereObject (idx)));
+        return normalize3 (substractXYZ (P, objects.getSphereObject (idx)));
     }
 
     public float[] planeNormal (int idx, float[] P, float[] O)
     {
-      int axis = (int)objects.getPlaneData (idx, 0);
-      float[] N = { 0.0f, 0.0f, 0.0f };
-      N [axis] = O [axis] - objects.getPlaneData (idx, 1);
-      return normalize3 (N);
+        int axis = (int)objects.getPlaneData (idx, 0);
+        float[] N = { 0.0f, 0.0f, 0.0f };
+        N [axis] = O [axis] - objects.getPlaneData (idx, 1);
+        return normalize3 (N);
     }
 
     public float[] surfaceNormal (int type, int index, float[] P, float[] Inside)
     {
-      if (type == 0) {
-        return sphereNormal (index, P);
-      } else {
-        return planeNormal (index, P, Inside);
-      }
+        if (type == 0) {
+            return sphereNormal (index, P);
+        } else {
+            return planeNormal (index, P, Inside);
+        }
     }
 
     public float lightObject (int type, int idx, float[] P, float lightAmbient)
     {
-      float i = lightDiffuse (surfaceNormal (type, idx, P, lightSource), P);
-      return  Mathf.Min (1.0f, Mathf.Max (i, lightAmbient));
+        float[] b = { 0.0f, 0.0f, 0.0f };
+        b[0] = lightPos.x;
+        b[1] = lightPos.y;
+        b[2] = lightPos.z;
+        float i = lightDiffuse (surfaceNormal (type, idx, P, b), P);
+        return  Mathf.Min (1.0f, Mathf.Max (i, lightAmbient));
     }
 
 
@@ -339,7 +384,6 @@ public class RayTracingMaster : MonoBehaviour
             _sphereBuffer.SetData(spheres);
         }
     }
-    */
 
     private void SetShaderParameters()
     {
